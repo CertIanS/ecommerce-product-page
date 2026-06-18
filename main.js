3a882a3db7a9e9ca707ee0f7bb610800a0fd9a4c
CertIanS <irsnider13@gmail.com>
2026-06-18 11:31:03 -0400

    Edited lightbox functionality

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var mobileMenu = document.getElementById("mobileMenu");
 var mainBody = document.querySelector("main");
+var slideIndex = 1;
 
 function showMenu(){
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
@@ -15,8 +16,43 @@ function hideMenu(){
 
 function openLightBox(){
     document.getElementById("lightBox").style.display = "block";
+    document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    mainBody.classList.add("open");
+    showSlides(slideIndex);
 }
 
 function closeLightBox(){
     document.getElementById("lightBox").style.display = "none";
+    document.body.style.backgroundColor = "white";
+    mainBody.classList.remove("open");
+}
+
+function moveSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    slideIndex = n;
+}
+
+function showCurrentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    var thumbs = document.getElementsByClassName("thumbnail");
+    if(n > slides.length)
+        slideIndex = 1;
+    if(n < 1)
+        slideIndex = slides.length;
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0; i < thumbs.length; i++){
+        thumbs[i].className = thumbs[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    thumbs[slideIndex-1].className += " active";
 }
